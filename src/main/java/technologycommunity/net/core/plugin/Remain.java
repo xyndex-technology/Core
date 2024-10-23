a7b67ddfc0fa547c3266cef2ffda38c2bf3d6296
x3xgit <xyndex.shuvar@gmail.com>
2024-10-23 17:54:54 +0300

    2.2.4

--- src/main/java/technologycommunity/net/core/plugin/Remain.java
+++ src/main/java/technologycommunity/net/core/plugin/Remain.java
@@ -1,16 +1,19 @@
 package technologycommunity.net.core.plugin;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandMap;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Field;
+import java.util.Set;
 
 public class Remain {
     public static boolean registerCommand(final @NotNull Command command, boolean ex) {
         final CommandMap map = Reflection.getCommandMap();
 
-        if (map.getKnownCommands().containsValue(command)) {
+        final Set<String> registeredCommands = Bukkit.getServer().getCommandAliases().keySet();
+        if (registeredCommands.contains(command.getLabel())) {
             if (ex)
                 Core.getInstance().getCoreLogger().warning("Couldn't register command because it's already been registered. (%s)".formatted(command.getLabel()));
             return false;
